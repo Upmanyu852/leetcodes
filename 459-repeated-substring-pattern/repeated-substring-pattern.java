1b7b67f578cd7ae5ca01485a1956d0409c9f8980
Upmanyu <upmanyu812@gmail.com>
2026-06-12 14:50:15 +0530

    Time: 76 ms (27.36%) | Memory: 47 MB (35.36%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+
+        String doubled = (s + s).substring(1, 2 * s.length() - 1);
+
+        return doubled.contains(s);
+    }
+}
