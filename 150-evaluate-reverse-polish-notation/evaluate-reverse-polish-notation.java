985b2e1a372ac8dea0786b6c7dde261ca5161b28
Upmanyu <upmanyu812@gmail.com>
2026-06-23 16:04:20 +0530

    Time: 6 ms (88.39%) | Memory: 45.3 MB (63.05%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -35,3 +35,93 @@ class Solution {
         return stack.peek();//last number 
     }
 }
+
+/* dry run
+LeetCode 150 - Evaluate Reverse Polish Notation
+
+Pattern: Stack (Expression Evaluation)
+
+Rule:
+Number aaye -> Push
+Operator aaye -> 2 values Pop -> Calculate -> Result Push
+End me stack ka top = Answer
+
+IMPORTANT:
+a = first pop
+b = second pop
+
+Result = b operator a
+
+NOT:
+a - b
+a / b
+
+Always:
+b - a
+b / a
+
+Dry Run:
+
+Input:
+["2","1","+","3","*"]
+
+2  -> [2]
+
+1  -> [2,1]
+
++  -> pop 1, pop 2
+      2+1=3
+      push 3
+      [3]
+
+3  -> [3,3]
+
+*  -> pop 3, pop 3
+      3*3=9
+      push 9
+      [9]
+
+Answer = 9
+
+Pseudo Logic:
+
+for each token
+
+    if operator
+        a = pop()
+        b = pop()
+
+        result = b op a
+
+        push(result)
+
+    else
+        push(Integer.parseInt(token))
+
+return stack.peek()
+
+Common Trap:
+
+Input:
+["4","2","-"]
+
+Stack:
+[4,2]
+
+a = pop() = 2
+b = pop() = 4
+
+Answer:
+b-a = 4-2 = 2
+
+NOT:
+a-b = 2-4 = -2
+
+Complexity:
+Time  = O(n)
+Space = O(n)
+
+One Line Memory:
+Number -> Push
+Operator -> Pop 2 -> b op a -> Push
+Top of Stack = Answer*/
